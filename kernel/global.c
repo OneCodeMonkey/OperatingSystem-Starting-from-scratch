b2f0f4f07834d3fcede139204c1bd08ad99df23e
OneCodeMonkey <1460018362@qq.com>
2019-06-16 03:28:21 +0800

    dd_map

--- kernel/global.c
+++ kernel/global.c
@@ -42,3 +42,22 @@ PUBLIC CONSOLE 	console_table[NR_CONSOLES];
 PUBLIC irq_handler	irq_table[NR_IRQ];
 
 PUBLIC system_call 	sys_call_table[NR_SYS_CALL]	= {sys_printx, sys_sendrec};
+
+/**
+ *	FS related below
+ *	
+ *	For dd map[k]
+ *	`k` is the device nr. \ dd_map[k]. driver_nr is the driver nr.
+ *
+ *	Remember to modify include/const.h if the order is changed.
+ */
+struct dev_drv_map dd_map[] = {
+	/* driver nr.	major device nr. */
+	/* ----------	---------------- */
+	{INVALID_DRIVER},	// <0: Unused
+	{INVALID_DRIVER},	// <1: Reserved for floppy driver
+	{INVALID_DRIVER},	// <2: Reserved for cdrom driver
+	{TASK_HD},			// <3: Hard disk
+	{TASK_TTY},			// <4: TTY
+	{INVALID_DRIVER},	// <5: Reserved for scsi disk driver
+};
