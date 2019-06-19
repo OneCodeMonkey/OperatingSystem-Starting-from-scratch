dda10a440ba3d97214e0f9b1390e29b68bc0e718
OneCodeMonkey <1460018362@qq.com>
2019-06-19 12:16:24 +0800

    const: hard-drive, device number, partitions

--- include/sys/const.h
+++ include/sys/const.h
@@ -189,3 +189,76 @@ enum msgtype{
 #define RETVAL u.m3.m3i1
 #define STATUS u.m3.m3i1
 
+
+
+//
+#define DIOCTL_GET_GEO 1
+// Hard Drive
+#define SECTOR_SIZE 512
+#define SECTOR_BITS (SECTOR_SIZE * 8)
+#define SECTOR_SIZE_SHIFT 9
+
+// major device numbers(corresponding to kernel/global.c::dd_map[])
+#define NO_DEV 0
+#define DEV_FLOPPY 1
+#define DEV_CDROM 2
+#define DEV_HD 3
+#define DEV_CHAR_TTY 4
+#define DEV_SCSI 5
+// make device number from major and minor numbers
+#define MAJOR_SHIFT 8
+#define MAKE_DEV(a,b) ((a << MAJOR_SHIFT) | b)
+// separate major and minor numbers from device number
+#define MAJOR(x) ((x >> MAJOR_SHIFT) & 0xFF)
+#define MINOR(x) (x & 0xFF)
+
+#define INVALID_INODE 0
+#define ROOT_INODE 1
+
+#define MAX_DRIVES 2
+#define NR_PART_PER_DRIVE 4
+#define NR_SUB_PER_PART 16
+#define NR_SUB_PER_DRIVE (NR_SUB_PER_PART * NR_PART_PER_DRIVE)
+#define NR_PRIM_PER_DRIVE (NR_PART_PER_DRIVE + 1)
+
+/**
+ * @def MAX_PRIM
+ * Defines the max minor number of the primary partitions.
+ * If there are two disks, prim_dev ranges in hd[0-9], this macro will equals tp 9.
+ */
+#define MAX_PRIM (MAX_DRIVES * NR_PRIM_PER_DRIVE - 1)
+
+#define MAX_SUBPARTITIONS (NR_SUB_PER_DRIVE * MAX_DRIVES)
+
+// device numbers of hard disk
+#define MINOR_hd1a 0x10
+#define MINOR_hd2a (MINOR_hd1a + NR_SUB_PER_PART)
+#define MINOR_hd2c (MINOR_hd1a + NR_SUB_PER_PART + 2)
+
+#define ROOT_DEV MAKE_DEV(DEV_HD, MINOR_BOOT)
+
+#define P_PRIMARY 0
+#define P_EXTENDED 1
+
+#define ORANGES_PART 0x99	// partition
+#define NO_PART 0x00 // unused entry
+#define EXT_PART 0x05 // extended partition
+
+#define NR_FILES 64
+#define NR_FILE_DESC 64 // FIXME
+#define NR_INODE 64 // FIXME
+#define NR_SUPER_BLOCK 8
+
+// INODE::i_mode(octal, lower 12 bits reserved)
+#define I_TYPE_MASK 0170000
+#define I_REGULAR 0100000
+#define I_BLOCK_SPECIAL 0060000
+#define I_DIRECTORY 0040000
+#define I_CHAR_SPECIAL 0020000
+#define I_NAMED_PIPE 0010000
+
+#define is_special(m) ((((m) & I_TYPE_MASK) == I_BLOCK_SPECIAL) || (((m) & I_TYPE_MASK) == I_CHAR_SPECIAL))
+
+#define NR_DEFAULT_FILE_SECTS 2048	// 2048 * 512 = 1MB
+
+#endif
