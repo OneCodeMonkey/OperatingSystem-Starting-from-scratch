7d459ef8e9f21d2cc8278439d564ceec3e2549ca
OneCodeMonkey <1460018362@qq.com>
2019-06-19 11:24:44 +0800

    hardware interrupt

--- include/sys/const.h
+++ include/sys/const.h
@@ -102,5 +102,17 @@
 						   	   2 = CMOS time invalid
 						   	   1,0 reserved
 						*/
+// Hardware interrupts
+#define NR_IRQ 16 // Number of IRQs
+#define CLOCK_IRQ 0
+#define KEYBOARD_IRQ 1
+#define CASCADE_IRQ 2	// cascade enable for 2nd AT controller
+#define ETHER_IRQ 3	// default ethernet interrupt vector
+#define SECONDARY_IRQ 3	// RS232 interrupt vector for port 2
+#define RS232_IRQ 4	// RS232 interrupt vector for port 1
+#define XT_WINI_IRQ 5	// xt winchester
+#define FLOPPY_IRQ 6	// floppy disk
+#define PRINTER_IRQ 7
+#define AT_WINI_IRQ 14	// at winchester
 
 
