4bf34a943158689dc292b4bd5586f7c51a583f07
OneCodeMonkey <1460018362@qq.com>
2019-06-22 04:46:00 +0800

    define task_sys

---
+++ kernel/systask.c
@@ -0,0 +1,26 @@
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "keyboard.h"
+#include "proto.h"
+
+PRIVATE int read_register(char reg_addr);
+PRIVATE u32 get_rtc_time(struct time *t);
+
+/**
+ * task_sys
+ *
+ * <Ring 1> The main loop of TASK SYS.
+ *
+ */
+PUBLIC void task_sys()
+{
+	// todo
+}
