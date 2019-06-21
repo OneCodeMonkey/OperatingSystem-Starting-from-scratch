db150def68de637dfa1c4f47a90a9d1511a5bb73
OneCodeMonkey <1460018362@qq.com>
2019-06-22 03:28:22 +0800

    itoa()

--- kernel/klib.c
+++ kernel/klib.c
@@ -83,3 +83,37 @@ PUBLIC int get_kernel_map(unsigned int *b, unsigned int *l)
 
 	return 0;
 }
+
+/**
+ * itoa()
+ * 数字前面的 0 不被显示出来，比如 0000B800 会被显示成 B800
+ */
+PUBLIC char* itoa(char* str, int num)
+{
+	char* p = str;
+	char ch;
+	int i;
+	int flag = 0;
+
+	*p++ = '0';
+	*p++ = 'x';
+
+	if(num == 0)
+		*p++ = '0';
+	else {
+		for(i = 28; i >= 0; i -= 4) {
+			ch = (num >> i) & 0xF;
+			if(flag || (ch > 0)) {
+				flag = 1;
+				ch += '0';
+				if(ch > '9') {
+					ch += 7;
+				}
+				*p++ = ch;
+			}
+		}
+	}
+	*p = 0;
+
+	return str;
+}
