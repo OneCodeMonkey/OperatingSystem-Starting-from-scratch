6223b2bd84f134b681b0ecb757b5f0e340000f4c
OneCodeMonkey <1460018362@qq.com>
2019-06-22 14:08:40 +0800

    memory-manage: do_exit()

--- mm/forkexit.c
+++ mm/forkexit.c
@@ -105,3 +105,80 @@ PUBLIC int do_fork()
 
 	return 0;
 }
+
+/**
+ * do_exit()
+ *
+ * Perform the exit() syscall.
+ *
+ * If proc A calls exit(), then MM will do the following in this routine:
+ *     <1> inform FS so that the fd-related things will be cleaned up
+ *     <2> free A's memory
+ *     <3> set A.exit_status, which is for the parent
+ *     <4> depends on parent's status. if parent (say P) is:
+ *           (1) WAITING
+ *                 - clean P's WAITING bit, and
+ *                 - send P a message to unblock it
+ *                 - release A's proc_table[] slot
+ *           (2) not WAITING
+ *                 - set A's HANGING bit
+ *     <5> iterate proc_table[], if proc B is found as A's child, then:
+ *           (1) make INIT the new parent of B, and
+ *           (2) if INIT is WAITING and B is HANGING, then:
+ *                 - clean INIT's WAITING bit, and
+ *                 - send INIT a message to unblock it
+ *                 - release B's proc_table[] slot
+ *               else
+ *                 if INIT is WAITING but B is not HANGING, then
+ *                     - B will call exit()
+ *                 if B is HANGING but INIT is not WAITING, then
+ *                     - INIT will call wait()
+ *
+ * TERMs:
+ *     - HANGING: everything except the proc_table entry has been cleaned up.
+ *     - WAITING: a proc has at least one child, and it is waiting for the
+ *                child(ren) to exit()
+ *     - zombie: say P has a child A, A will become a zombie if
+ *         - A exit(), and
+ *         - P does not wait(), neither does it exit(). that is to say, P just
+ *           keeps running without terminating itself or its child
+ * 
+ * @param status  Exiting status for parent.
+ * 
+ */
+PUBLIC void do_exit(int status)
+{
+	int i;
+	int pid = mm_msg.source;	/* PID of caller */
+	int parent_pid = proc_table[pid].p_parent;
+	struct proc* p = &proc_table[pid];
+
+	/* tell FS, see fs_exit() */
+	MESSAGE msg2fs;
+	msg2fs.type = EXIT;
+	msg2fs.PID = pid;
+	send_recv(BOTH, TASK_FS, &msg2fs);
+
+	free_mem(pid);
+
+	p->exit_status = status;
+
+	if(proc_table[parent_pid].p_flags & WAITING) { /* parent is waiting */
+		proc_table[parent_pid] &= ~WAITING;
+		cleanup(&proc_table[pid]);
+	} else { /* parent is not waiting */
+		proc_table[pid].p_flags |= HANGING;
+	}
+
+	/* if the proc has any child, make INIT the new parent */
+	for(i = 0; i < NR_TASKS + NR_PROCS; i++) {
+		if(proc_table[i].p_parent == pid) {	/* is a child */
+			proc_table[i].p_parent = INIT;
+			if((proc_table[INIT].p_flags & WAITING) && \
+				(proc_table[i].p_flags & HANGING)) {
+				proc_table[INIT].p_flags &= ~WAITING;
+				cleanup(&proc_table[i]);
+			}
+		}
+	}
+}
