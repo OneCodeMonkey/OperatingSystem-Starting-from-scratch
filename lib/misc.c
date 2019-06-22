e234a99f1fba930ef2c17af53fae09c04ef7b7dc
OneCodeMonkey <1460018362@qq.com>
2019-06-22 23:50:15 +0800

    strcat()

--- lib/misc.c
+++ lib/misc.c
@@ -114,3 +114,34 @@ PUBLIC int strcmp(const char* s1, const char* s2)
 	return (*p1 - *p2);
 }
 
+/**
+ * strcat()
+ *
+ * Concatenate two strings.
+ *
+ * @param s1: The 1st string.
+ * @param s2: The 2nd string.
+ * @return Ptr: to the 1st string.
+ *
+ */
+PUBLIC char* strcat(char* s1, const char* s2)
+{
+	if((s1 == 0) || (s2 == 0)) {	/* for stablity( robustness) */
+		return 0;
+	}
+
+	char* p1 = s1;
+
+	for(; *p1; p1++) {
+		//
+	}
+
+	const char* p2 = s2;
+	for(; *p2; p1++, p2++) {
+		*p1 = *p2;
+	}
+	*p1 = 0;
+
+	return s1;
+}
+
