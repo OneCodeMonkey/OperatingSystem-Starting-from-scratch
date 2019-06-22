82177b0bec8b7fda3b21db6335bfe5682c3fb67f
OneCodeMonkey <1460018362@qq.com>
2019-06-22 23:15:21 +0800

    getpid()

---
+++ lib/getpid.c
@@ -0,0 +1,31 @@
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "proto.h"
+
+/**
+ * getpid()
+ *
+ * Get pid
+ *
+ * @return PID.
+ *
+ */
+PUBLIC int getpid()
+{
+	MESSAGE msg;
+	msg.type = GET_PID;
+
+	send_recv(BOTH, TASK_SYS, &msg);
+	assert(msg.type == SYSCALL_RET);
+
+	return msg.PID;
+}
+
