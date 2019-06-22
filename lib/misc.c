f921ab90639350f40ce05bed734b2161daacc3b2
OneCodeMonkey <1460018362@qq.com>
2019-06-22 23:48:02 +0800

    strcmp()

--- lib/misc.c
+++ lib/misc.c
@@ -84,3 +84,33 @@ PUBLIC int memcmp(const void* s1, const void* s2, int n)
 	return 0;
 }
 
+/**
+ * strcmp()
+ *
+ * Compare two strings.
+ *
+ * @param s1: The 1st string.
+ * @param s2: The 2nd string.
+ *
+ * @return an integer less than or equal to or greater than zero if s1(or the first
+ *		n bytes thereof) is found, respectively, to be less than, to match, or
+ *		be greater than s2.
+ *
+ */
+PUBLIC int strcmp(const char* s1, const char* s2)
+{
+	if((s1 == 0) || (s2 == 0)) {	/* for stablity( robustness) */
+		return (s1 - s2);
+	}
+
+	const char* p1 = s1;
+	const char* p2 = s2;
+
+	for(; *p1 && *p2; p1++, p2++) {
+		if(*p1 != *p2)
+			break;
+	}
+
+	return (*p1 - *p2);
+}
+
