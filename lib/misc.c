ca1537cefeceab7cf8fb1a0e043fd0b692641ca4
OneCodeMonkey <1460018362@qq.com>
2019-06-22 23:55:42 +0800

    assert failure

--- lib/misc.c
+++ lib/misc.c
@@ -157,3 +157,36 @@ PUBLIC void spin(char* func_name)
 		//
 	}
 }
+
+/**
+ * assertion failure
+ *
+ * Invoke by assert().
+ *
+ * @param exp: The failure expression itself.
+ * @param file: __FILE__
+ * @param base_file: __BASE_FILE__
+ * @param line: __LINE__
+ *
+ */
+PUBLIC void assertion_failure(char* exp, char* file, char* base_file, int line)
+{
+	printl("%c assert(%s) failed: file: %s, base_file: %s, ln%d", MAG_CH_ASSERT, \
+		exp, file, base_file, line);
+
+	/**
+	 * If assertion fails in a TASK, the system will halt before printl() returns.
+	 * If it happens in a USER PROC, printl() will return like a common routine
+	 * and arrive here.
+	 * 
+	 * @see sys_printx()
+	 *
+	 * We use a forever loop to prevent the proc from going on:
+	 *
+	 */
+	spin("assertion_failure()");
+
+	/* should never arrive here */
+	__asm__ __volatile__("ud2");
+}
+
