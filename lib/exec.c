2b0137961dfdf45fbe2c164679b831e85e34e5cd
OneCodeMonkey <1460018362@qq.com>
2019-06-22 23:04:58 +0800

    execv()

--- lib/exec.c
+++ lib/exec.c
@@ -44,3 +44,47 @@ PUBLIC int execl(const char* path, const char* arg, ...)
 	char** p = (char**)parg;
 	return execv(path, p);
 }
+
+/**
+ * execv()
+ *
+ */
+PUBLIC int execv(const char* path, char* argv[])
+{
+	char** p = argv;
+	char arg_stack[PROC_ORIGIN_STACK];
+	int stack_len = 0;
+
+	while(*p++) {
+		assert(stack_len + 2 * sizeof(char*) < PROC_ORIGIN_STACK);
+		stack_len += sizeof(char*);
+	}
+
+	*((int*)(&arg_stack[stack_len])) = 0;
+	stack_len += sizeof(char*);
+
+	char** q = (char**)arg_stack;
+
+	for(p = argv; *p != 0; p++) {
+		*q++ = &arg_stack[stack_len];
+
+		assert(stack_len + strlen(*p) + 1 < PROC_ORIGIN_STACK);
+		strcpy(&arg_stack[stack_len], *p);
+		stack_len += strlen(*p);
+		arg_stack[stack_len] = 0;
+		stack_len++;
+	}
+
+	MESSAGE msg;
+	msg.type = EXEC;
+	msg.PATHNAME = (void*)path;
+	msg.NAME_LEN = strlen(path);
+	msg.BUF = (void*)arg_stack;
+	msg.BUF_LEN = stack_len;
+
+	send_recv(BOTH, TASK_MM, &msg);
+	assert(msg.type == SYSCALL_RET);
+
+	return msg.RETVAL;
+}
+
