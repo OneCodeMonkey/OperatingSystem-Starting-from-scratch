8cb50b9ac829e3acff3e3bc42ddbabd1b0122a3c
OneCodeMonkey <1460018362@qq.com>
2019-06-23 20:47:00 +0800

    vsprintf()

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -28,3 +28,93 @@ PRIVATE char* i2a(int val, int base, char** ps)
 
 	return *ps;
 }
+
+/**
+ * vsprintf()
+ *
+ * 为了更好地理解 vsprintf() 的含义，可以参考 printf() 的实现部分
+ *
+ */
+PUBLIC int vsprintf(char* buf, const char* fmt, va_list args)
+{
+	char* p;
+
+	va_list p_next_arg = args;
+	int m;
+
+	char inner_buf[STR_DEFAULT_LEN];
+	char cs;
+	int align_nr;
+
+	for(p = buf; *fmt; fmt++) {
+		if(*fmt != '%') {
+			*p++ = *fmt;
+			continue;
+		} else { 	/* a format string begins */
+			align_nr = 0;
+		}
+
+		fmt++;
+
+		if(*fmt == '%') {
+			*p++ = *fmt;
+			continue;
+		} else if (*fmt == '0') {
+			cs = '0';
+			fmt++;
+		} else {
+			cs = ' ';
+		}
+
+		while(((unsigned char)(*fmt) >= '0') && ((unsigned char)(*fmt) <= '9')) {
+			align_nr *= 10;
+			align_nr += *fmt - '0';
+			fmt++;
+		}
+
+		char* q = inner_buf;
+		memset(q, 0, sizeof(inner_buf));
+
+		switch(fmt) {
+			case 'c':
+				*q++ = *((char*)p_next_arg);
+				p_next_arg += 4;
+				break;
+			case 'x':
+				m = *((int*)p_next_arg);
+				i2a(m, 16, &q);
+				p_next_arg += 4;
+				break;
+			case 'd':
+				m = *((int*)p_next_arg);
+				if(m < 0) {
+					m = -m;
+					*q++ = '-';
+				}
+				i2a(m, 10, &q);
+				p_next_arg += 4;
+				break;
+			case 's':
+				strcpy(q, (*((char**)p_next_arg)));
+				q += strlen(*((char**)p_next_arg));
+				p_next_arg += 4;
+				break;
+			default:
+				break;		
+		}
+
+		int k;
+		for(k = 0; k < ((align_nr > strlen(inner_buf)) ? (align_nr - strlen(inner_buf)) : 0); k++) {
+			*p++ = cs;
+		}
+		q = inner_buf;
+		while(*q) {
+			*p++ = *q++;
+		}
+	}
+
+	*p = 0;
+
+	return (p - buf);
+}
+
