8e0da473b12070159443efabed8fe354537713c9
OneCodeMonkey <1460018362@qq.com>
2019-06-25 23:33:50 +0800

    kernel: disp_str()

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,3 +1,63 @@
 ; ------------------------------------------------------------------------
 ; kernel.asm
 ; ------------------------------------------------------------------------
+
+%include "sconst.inc"
+
+; 导入全局变量
+extern disp_pos
+
+[SECTION .text]
+
+; 导出函数
+global disp_str
+global disp_color_str
+global out_byte
+global in_byte
+global enable_irq
+global disable_irq
+global enable_int
+global disable_int
+global port_read
+global port_write
+global glitter
+
+; ------------------------------------------------------------------------
+; void disp_str(char* info);
+; ------------------------------------------------------------------------
+disp_str:
+	push ebp
+	mov ebp, esp
+
+	mov esi, [ebp + 8]		; pszInfo
+	mov edi, [disp_pos]
+	mov ah, 0Fh
+
+.1:
+	lodsb
+	test 	al, al
+	jz .2
+	cmp al, 0Ah		; 判断是否回车
+	jnz .3
+	push eax
+	mov eax, edi
+	mov bl, 160
+	div bl
+	and eax, 0FFh
+	inc eax
+	mov bl, 160
+	mul bl
+	mov edi, eax
+	pop eax
+	jmp .1
+
+.3:
+	mov [gs:edi], ax
+	add edi, 2
+	jmp .1
+
+.2:
+	mov [disp_str], edi
+	pop ebp
+	ret		; 函数结束，退出
+; ------------------------------------------------------------------------
