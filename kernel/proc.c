1f268afd757e22966ed3ef277e6e0f8c8cfdf8f7
OneCodeMonkey <1460018362@qq.com>
2019-06-26 23:52:27 +0800

    sys_sendrec()

--- kernel/proc.c
+++ kernel/proc.c
@@ -42,3 +42,51 @@ PUBLIC void schedule()
 					p->ticks = p->priority;
 	}
 }
+
+/**
+ * sys_sendrec
+ *
+ * <Ring 0> The core routine of system call `sendrec()`
+ *
+ * @param function: SEND or RECEIVE
+ * @param src_dest: To or From whom the message is transferred
+ * @param m: Ptr to the MESSAGE body
+ * @param p: The caller proc.
+ * @return 0 if success.
+ *
+ */
+PUBLIC int sys_sendrec(int function, int src_dest, MESSAGE* m, struct proc* p)
+{
+	assert(k_reenter == 0);		/* make sure we are not in ring 0. */
+	assert((src_dest >= 0 && src_dest < NR_TASKS + NR_PROCS) || \
+		src_dest == ANY || src_dest == INTERRUPT);
+
+	int ret = 0;
+	int caller = proc2pid(p);
+	MESSAGE* mla = (MESSAGE*)va2la(caller, m);
+	mla->source = caller;
+
+	assert(mla->source != src_dest);
+
+	/**
+	 * Actually we have the third message type: BOTH, However, it is not 
+	 * allowed to be passed to the kernel directly. Kernel doesn't know it
+	 * at all. It is transformed into a SEND followed by a RECEIVE by 
+	 * `send_recv()`
+	 *
+	 */
+	if(function == SEND) {
+		ret = msg_send(p, src_dest, m);
+		if(ret != 0)
+			return ret;
+	} else if(function == RECEIVE) {
+		ret = msg_receive(p, src_dest, m);
+		if(ret != 0)
+			return ret;
+	} else {
+		panic("{sys_sendrec} invalid function: %d (SEND:%d, RECEIVE:%d).", \
+			function, SEND, RECEIVE);
+	}
+
+	return 0;
+}
