3950c38e84bc4c00b85cff96bae6053349061383
OneCodeMonkey <1460018362@qq.com>
2019-06-26 22:25:01 +0800

    kernel: main.c

---
+++ kernel/main.c
@@ -0,0 +1,22 @@
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "proto.h"
+
+/**
+ * kernel_main
+ *
+ * jmp from kernel.asm::_start.
+ *
+ */
+PUBLIC int kernel_main()
+{
+	// todo
+}
