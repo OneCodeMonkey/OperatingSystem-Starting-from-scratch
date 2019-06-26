a626d613ef1367d4d25124b1953cec01b7e93573
OneCodeMonkey <1460018362@qq.com>
2019-06-26 20:10:52 +0800

    klib: disable_irq()

--- kernel/kliba.asm
+++ kernel/kliba.asm
@@ -150,3 +150,52 @@ port_write:
 	rep outsw
 	ret 		; return
 ; ------------------------------------------------------------------------
+
+; ------------------------------------------------------------------------
+; void disable_irq(int irq);
+; ------------------------------------------------------------------------
+; Disable an interrupt request line by setting an 8259 bit.
+; Equivalent code:
+; 	if(irq < 8) {
+;		out_byte(INT_M_CTLMASK, in_byte(INT_M_CTLMASK) | (1 << irq));
+;	} else {
+;		out_byte(INT_S_CTLMASK, in_byte(INT_S_CTLMASK) | (1 << irq));
+;	}
+
+disable_irq:
+	mov ecx, [esp + 4]		; irq
+	push f
+	cli
+	mov ah, 1
+	rol ah, cl 		; ah = (1 << (irq % 8))
+	cmp cl, 8
+	jae disable_8	; disable irq >= 8 at the slave 8259
+
+disable_0:
+	in al, INT_M_CTLMASK
+	test al, ah
+	jnz dis_already		; already disabled ?
+	or al, ah
+	out INT_M_CTLMASK, al 	; set bit at master 8259
+	popf
+	mov eax, 1 			; disabled by this function
+	ret 		; return
+
+disable_8:
+	in al, INT_S_CTLMASK
+	test al, ah
+	jnz dis_already 	; already disabled ?
+	or al, ah
+	out INT_S_CTLMASK, al 	; set bit at slave 8259
+	popf
+	mov eax, 1 			; disabled by this function
+	ret 		; return
+
+dis_already:
+	popf
+	xor eax, eax 		; already disabled
+	ret 		; return
+
+; ------------------------------------------------------------------------
+
+
