5174b93978ef6b2b73a0312bd5f7ed2a45e9c869
OneCodeMonkey <1460018362@qq.com>
2019-06-26 22:44:21 +0800

    get ticks

--- kernel/main.c
+++ kernel/main.c
@@ -114,3 +114,15 @@ PUBLIC int kernel_main()
 		//
 	}
 }
+
+/**
+ * get_ticks
+ */
+PUBLIC int get_ticks()
+{
+	MESSAGE msg;
+	reset_msg(&msg);
+	msg.type = GET_TICKS;
+	send_recv(BOTH, TASK_SYS, &msg);
+	return msg.RETVAL;
+}
