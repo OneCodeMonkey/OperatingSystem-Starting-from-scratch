8c4561253d5d8d1e33a9e0d98b7ba32fa6de9750
OneCodeMonkey <1460018362@qq.com>
2019-06-26 22:49:17 +0800

    struct define: posix_tar_header

--- kernel/main.c
+++ kernel/main.c
@@ -126,3 +126,30 @@ PUBLIC int get_ticks()
 	send_recv(BOTH, TASK_SYS, &msg);
 	return msg.RETVAL;
 }
+
+/**
+ * @struct posix_tar_header
+ * 
+ * Copied from GNU `tar`
+ *
+ */
+struct posix_tar_header {
+	char name[100];		/* byte offset 0 */
+	char mode[8];		/*			   100 */
+	char uid[8];		/*			   108 */
+	char gid[8];		/*			   116 */
+	char size[12];		/*			   124 */
+	char mtime[12];		/*			   136 */
+	char chksum[8];		/*			   148 */
+	char typeflag;		/*			   156 */
+	char linkname[100];	/*			   157 */
+	char magic[6];		/*			   257 */
+	char version[2];	/*			   263 */
+	char uname[32];		/*			   265 */
+	char gname[32];		/*			   297 */
+	char devmajor[8];	/*			   329 */
+	char devminor[8];	/*			   337 */
+	char prefix[155];	/*			   345 */
+
+	/* 500 */
+}
