172523df99b0e57abb05d06bf0a3983ad3314925
OneCodeMonkey <1460018362@qq.com>
2019-06-28 02:19:18 +0800

    kernel/proc: inform_int()

--- kernel/proc.c
+++ kernel/proc.c
@@ -441,3 +441,35 @@ PRIVATE int msg_receive(struct proc* current, int src, MESSAGE* m)
 
 	return 0;
 }
+
+/**
+ * inform_int
+ *
+ * <Ring 0> Inform a proc that an interrupt has occured.
+ * 
+ * @param task_nr: The task which will be informed.
+ *
+ */
+PUBLIC void inform_int(int task_nr)
+{
+	struct proc* p = proc_table + task_nr;
+
+	if((p->p_flags & RECEIVING) && \	/* dest is waiting for the msg */
+		((p->p_recvfrom == INTERRUPT) || (p->p_recvfrom == ANY))) {
+		p->p_msg->source = INTERRUPT;
+		p->p_msg->type = HARD_INT;
+		p->p_msg = 0;
+		p->has_int_msg = 0;
+		p->p_flags &= ~RECEIVING;	/* dest has received the msg */
+		p->p_recvfrom = NO_TASK;
+		assert(p->p_flags == 0);
+		unblock(p);
+
+		assert(p->p_flags == 0);
+		assert(p->p_msg == 0);
+		assert(p->p_recvfrom == NO_TASK);
+		assert(p->p_sendto == NO_TASK);
+	} else {
+		p->has_int_msg = 1;
+	}
+}
