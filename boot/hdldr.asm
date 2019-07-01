812763fb0b3d0ae0fa0418285a2a2f1458a981e4
OneCodeMonkey <1460018362@qq.com>
2019-07-01 23:59:13 +0800

    get_inode()

--- boot/hdldr.asm
+++ boot/hdldr.asm
@@ -251,3 +251,45 @@ read_sector:
 
 	ret
 ; ------------------------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; get_inode
+; before:
+; 		- eax: inode nr.
+; after:
+;		- eax: sector nr.
+;		- ecx: the_inode.i_size
+;		- es:ebx: inodes sector buffer
+; registers changed:
+;		- eax, ebx, ecx, edx
+; ------------------------------------------------------------------------
+get_inode:
+	dec eax			; eax <- inode_nr - 1
+	mov bl, [fs:SB_INODE_SIZE]
+	mul bl 			; eax <- (inode_nr - 1) * INODE_SIZE
+	mov edx, SECT_BUF_SIZE
+	sub edx, dword [fs:SB_INODE_SIZE]
+	cmp eax, edx
+	jg err
+	push eax
+
+	mov ebx, [fs:SB_NR_IMAP_SECTS]
+	mov edx, [fs:SB_NR_SMAP_SECTS]
+	lea eax, [ebx+edx+ROOT_BASE+2]
+	mov dword [disk_address_packet + 8], eax
+	call read_sector
+
+	pop eax			; [es:ebx+eax] -> the inode
+
+	mov edx, dword [fs:SB_INODE_ISIZE_OFF]
+	add edx, ebx
+	add edx, eax	; [es:edx] -> the_inode.i_size
+	mov ecx, [es:edx]	; ecx <- the_inode.i_size
+
+	add ax, word [fs:SB_INODE_START_OFF]	; es:[ebx+eax] -> the_inode.i_start_sect
+	add bx, ax
+	mov eax, [es:bx]
+	add eax, ROOT_BASE		; eax <- the_inode.i_start_sect
+	ret 		; return
+; ------------------------------------------------------------------------
