a55a2933fcd82923528971e277021b1e3d8d2bce
OneCodeMonkey <1460018362@qq.com>
2019-07-04 22:20:55 +0800

    boot/hdldr.asm: InitKernel()

--- boot/hdldr.asm
+++ boot/hdldr.asm
@@ -717,3 +717,34 @@ SetupPaging:
 	nop 		; 一点延迟
 	ret 		; return
 ; ------------------------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; InitKernel
+; ------------------------------------------------------------------------
+InitKernel:	; 遍历每一个 Program Header, 根据 Program Header 中的信息来确定把什么放进内存，放到什么位置，以及放多少。
+	xor esi, esi
+	mov cx, word [KERNEL_FILE_PHY_ADDR + 2Ch]	; ┓ ecx <- pELFHdr->e_phnum
+	movzx ecx, cx 								; ┛
+	mov esi, [KERNEL_FILE_PHY_ADDR + 1Ch]		; esi <- pELFHdr->e_phoff
+	add esi, KERNEL_FILE_PHY_ADDR				; esi <- OffsetOfKernel + pELFHdr->e_phoff
+.Begin:
+	mov eax, [esi + 0]				
+	cmp eax, 0				; PT_NULL
+	jz .NoAction
+	push dword [esi + 010h] 	; size  ┓
+	mov eax, [esi + 04h]		;		┃
+	add eax, KERNEL_FILE_PHY_ADDR	; 	┣ ::memcpy((void*)(pPHdr->p_vaddr)),
+	push eax 				; src 	; 	┃ uchCode + pPHdr->p_offset,
+	push dword [esi + 08h]	; dst 		┃ pPHdr->p_filesze
+	call MemCpy 			; 			┃
+	add esp, 12				;			┛
+.NoAction:
+	add esi, 020h 			; esi += PELFHdr->e_phentsize
+	dec ecx
+	jnz .Begin
+
+	ret 			; return
+; InitKernel
+
+; ------------------------------------------------------------------------
