6c54b056b925fc7ff44d6dbfb750a91400ec7c37
OneCodeMonkey <1460018362@qq.com>
2019-07-04 21:26:50 +0800

    boot/hdldr.asm: DispStr()

--- boot/hdldr.asm
+++ boot/hdldr.asm
@@ -468,3 +468,49 @@ DispInt：
 	ret 			; return
 ; ------------------------------------------------------------------------
 
+
+; ------------------------------------------------------------------------
+; 函数: DispStr()
+; ------------------------------------------------------------------------
+DispStr:
+	push ebp
+	mov ebp, esp
+	push ebx
+	push esi
+	push edi
+
+	mov esi, [ebp + 8]		; pszInfo
+	mov edi, [dwDispPos]
+	mov ah, 0Fh 
+
+.1:
+	lodsb
+	test al, al
+	jz .2
+	cmp al, 0Ah 	; 判断是否按了回车
+	jnz .3
+	push eax
+	mov eax, edi
+	mov bl, 160
+	div bl
+	and eax, 0FFh 
+	inc eax
+	mov bl, 160
+	mul bl
+	mov edi, eax
+	pop eax
+	jmp .1
+
+.3:
+	mov [gs:edi], ax
+	add edi, 2
+	jmp .1
+
+.2:
+	mov [dwDispPos], edi
+	pop edi
+	pop esi
+	pop ebx
+	pop ebp
+	ret 		; return
+; ------------------------------------------------------------------------
