a02baa134301e5ef71d42a66fbf78346d5542a47
OneCodeMonkey <1460018362@qq.com>
2019-07-05 21:35:33 +0800

    boot/loader.asm: define GDT

--- boot/loader.asm
+++ boot/loader.asm
@@ -9,3 +9,14 @@ org 0100h
 %include "load.inc"
 %include "pm.inc"
 
+; ------------------------------------------------------------------------
+; GDT
+; ------------------------------------------------------------------------
+LABEL_GDT:	Descriptor	0,	0,	0	; 空描述符
+LABEL_DESC_FLAT_C:	Descriptor	0, 0fffffh,	DA_CR | DA_32 | DA_LIMIT_4K	; 0 ~ 4G
+LABEL_DESC_FLAT_RW:	Descriptor	0, 0fffffh, DA_DRW | DA_32 | DA_LIMIT_4K	; 0 ~ 4G
+LABEL_DESC_VIDEO: 	Descriptor	0B8000h, 0ffffh, DA_DRW | DA_DPL3	; 显存首地址
+
+; ------------------------------------------------------------------------
+
+
