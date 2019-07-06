60ba866ef22b93533d881800efc0bb7851e2f87b
OneCodeMonkey <1460018362@qq.com>
2019-07-06 13:24:27 +0800

    boot/loader.asm: LABEL_START

--- boot/loader.asm
+++ boot/loader.asm
@@ -32,3 +32,41 @@ SelectorFlatC equ LABEL_DESC_FLAT_C - LABEL_GDT
 SelectorFlatRW equ LABEL_DESC_FLAT_RW - LABEL_GDT
 SelectorVideo equ LABEL_DESC_VIDEO - LABEL_GDT + SA_RPL3
 ; ------------------------------------------------------------------------
+
+
+BaseOfStack equ 0100h
+
+LABEL_START:			; start loading
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+	mov sp, BaseOfStack
+
+	mov dh, 0		; "Loading  "
+	call DispStrRealMode		; 显示字符串
+
+	; 得到内存数
+	mov ebx, 0		; ebx = 后续值，开始时需要为0
+	mov di, _MemChkBuf	; es:di 指向一个地址范围描述符结构（Address Rang Descriptor Structure）
+
+.MemChkLoop:
+	mov eax, 0E820h		; eax = 0000E820h
+	mov ecx, 20			; ecx = 地址范围描述符结构的大小
+	mov edx, 0534D4150h	; edx = 'SMAP'
+	int 15h				; int 15h
+	jc .MemChkFail
+	add di, 20
+	inc dword [_dwMCRNumber]	; dwMCRNumber = ARDS 的个数
+	cmp ebx, 0
+	jne .MemChkLoop
+	jmp .MemChkOK
+.MemChkFail:
+	mov dword [_dwMCRNumber], 0
+.MemChkOK:
+	; 下面在 A盘根目录找 KERNEL.BIN
+	mov word [wSectorNo], SectorNoOfRootDirectory
+	xor ah, ah			; ┓
+	xor dl, dl 			; ┣ 软驱复位
+	int 13h				; ┛
+
