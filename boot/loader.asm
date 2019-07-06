3c44e98ac63a012113ce087c2ead7ead6e9e840e
OneCodeMonkey <1460018362@qq.com>
2019-07-07 01:37:11 +0800

    boot/loader.asm: InitKernel()

--- boot/loader.asm
+++ boot/loader.asm
@@ -805,3 +805,34 @@ SetupPaging:
 	nop 	; 少量延迟
 
 ; 分页机制启动完成---------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; 函数名：InitKernel()
+; 作用：将 KERNEL.BIN 的内容经过整理对齐后放到新的位置
+; ------------------------------------------------------------------------
+InitKernel:			; 遍历每一个 Program Header, 根据 Program Header 中的信息来确定把什么放进内存，放到什么位置，以及放多少进去
+	xor esi, esi
+	mov cx, word [KERNEL_FILE_PHY_ADDR + 2Ch]	; ┓ ecx <- pELFHdr->e_phnum
+	movzx ecx, cx 								; ┛
+	mov esi, [KERNEL_FILE_PHY_ADDR + 1Ch]		; esi <- pELFHdr->e_phoff
+	add esi, KERNEL_FILE_PHY_ADDR			; esi <- OffsetOfKernel + pELFHdr->e_phoff
+.Begin:
+	mov eax, [esi + 0]
+	cmp eax, 0 			; PT_NULL
+	jz .NoAction
+	push dword [esi + 010h]			; size ┓
+	mov eax, [esi + 04h]			;      ┃
+	add eax, KERNEL_FILE_PHY_ADDR	; 	   ┣ ::memcpy((void*)(pPHdr->p_vaddr), uchCode + pPHdr->p_offset, pPHdr->p_filesz);
+	push eax 						; src  ┃
+	push dword [esi + 08h]			;      ┃
+	call MemCpy 					;      ┃
+	add esp, 12						;      ┛
+.NoAction:
+	add esi, 020h 		; esi += pELFHdr->e_phentsize
+	dec ecx
+	jnz .Begin
+
+	ret 		; return
+; ------------------------------------------------------------------------
+
