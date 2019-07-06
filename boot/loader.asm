b89d0efd6ff40b9e1a35b5590771e3045171e992
OneCodeMonkey <1460018362@qq.com>
2019-07-07 00:58:37 +0800

    boot/loader.asm: DispStr()

--- boot/loader.asm
+++ boot/loader.asm
@@ -561,3 +561,47 @@ DispInt:
 	ret 		; return
 ; ------------------------------------------------------------------------
 
+
+; ------------------------------------------------------------------------
+; 函数名：DispStr()
+; ------------------------------------------------------------------------
+DispStr:
+	push ebp
+	mov ebp, esp
+	push ebx
+	push esi
+	push edi
+
+	mov esi, [ebp + 8]		; pszInfo
+	mov edi, [dwDispPos]
+	mov al, 0Fh
+.1:
+	lodsb
+	test al, al
+	jz .2
+	cmp al, 0Ah 		; 判断是否按了回车
+	jnz .3
+	push eax
+	mov eax, edi
+	mov bl, 160
+	div bl
+	and eax, 0FFh
+	inc eax
+	mov bl, 160
+	mul bl
+	mov edi, eax
+	pop eax
+	jmp .1
+.3:
+	mov [gs:edi], ax
+	add edi, 2
+	jmp .1
+.2:
+	mov [dwDispPos], edi
+	pop edi
+	pop esi
+	pop ebx
+	pop ebp
+	ret 			; return
+; ------------------------------------------------------------------------
+
