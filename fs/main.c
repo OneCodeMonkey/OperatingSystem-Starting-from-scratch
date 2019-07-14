882deb098c181b4af82a5dbe39f6088e1078e596
OneCodeMonkey <1460018362@qq.com>
2019-07-15 00:02:05 +0800

    fileSystem/main.c: initialize

---
+++ fs/main.c
@@ -0,0 +1,20 @@
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "config.h"
+#include "proto.h"
+
+#include "hd.h"
+
+PRIVATE void init_fs();
+PRIVATE void mkfs();
+PRIVATE void read_super_block(int dev);
+PRIVATE int fs_fork();
+PRIVATE int fs_exit();
