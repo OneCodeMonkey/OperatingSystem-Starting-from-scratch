bbb6b7c8c8d725dee58f5526ae48d34d3aba4c9e
OneCodeMonkey <1460018362@qq.com>
2019-07-14 22:03:36 +0800

    fileSystem/misc.c: strip_path()

--- fs/misc.c
+++ fs/misc.c
@@ -109,3 +109,53 @@ PUBLIC int search_file(char* path)
 
 	return 0;	/* file not found */
 }
+
+/**
+ * strip_path()
+ *
+ * Get the basename from the fullpath.
+ * In Orange's FS v1.0, all files are stored in the root directory.
+ * There is no sub-folder thing.
+ * This routine should be called at the very beginning of file operations.
+ * such as open(), read() and write(). It accepts the full path and returns two things: the basename and a ptr of the root dir's i-node.
+ *
+ * e.g. After strip_path(filename, '/blah', ppinode) finishes, we get:
+ *     
+ *    - filename: "blah"
+ *    - *ppinode: root_inode
+ *    - ret val: 0 (success)
+ *
+ * Current an acceptable pathname should begin with at most one `/` preceding a filename.
+ *
+ * Filenames may contain any character except '/' and '\\0'.
+ *
+ * @param[out] filename: The string for the result.
+ * @param[in] pathname: The full pathname.
+ * @param[out] ppinode: The ptr of the dir's inode will be stored here.
+ * @return Zero if success, -1 when pathname is not valid.
+ *
+ */
+PUBLIC int strip_path(char* filename, const char* pathname, struct inode** ppinode)
+{
+	const char* s = pathname;
+	char* t = filename;
+
+	if(s == 0)
+		return -1;
+	if(*s == '/')
+		s++;
+	while(*s) {		/* check each character */
+		if(*s == '/')
+			return -1;
+		*t++ = *s++;
+
+		/* if filename is too long, just truncate it */
+		if(t - filename >= MAX_FILENAME_LEN)
+			break;
+	}
+	*t = 0;
+
+	*ppinode = root_inode;
+
+	return 0;
+}
