1a9415e02965931be71a6609fbf4b74abaf50c13
OneCodeMonkey <1460018362@qq.com>
2019-07-14 21:55:30 +0800

    fileSystem/misc.c: search_file()

--- fs/misc.c
+++ fs/misc.c
@@ -62,3 +62,50 @@ PUBLIC int do_stat()
 
 	return 0;
 }
+
+/**
+ * search_file()
+ *
+ * Search the file and return the inode_nr.
+ *
+ * @param[in] path: the full path of the file to search.
+ * @return: Ptr to the i-node of the file if success, otherwise Zero.
+ *
+ * @see open()
+ * @see do_open()
+ *
+ */
+PUBLIC int search_file(char* path)
+{
+	int i ,j;
+	char filename(MAX_PATH);
+	memset(filename, 0, MAX_FILENAME_LEN);
+	struct inode* dir_inode;
+	if(strip_path(filename, path, &dir_inode) != 0)
+		return 0;
+	if(filename[0] == 0)
+		return dir_inode->i_num;
+
+	/* search the dir for the file. */
+	int dir_blk0_nr = dir_inode->i_start_sect;
+	int nr_dir_blks = (dir_inode->i_size + SECTOR_SIZE - 1) / SECTOR_SIZE;
+	int nr_dir_entries = dir_inode->i_size / DIR_ENTRY_SIZE;	/* including unused slots(the file has benn deleted but the slot is still there). */
+
+	int m = 0;
+	struct dir_entry* pde;
+	for(i = 0; i < nr_dir_blks; i++) {
+		RD_SECT(dir_inode->i_dev, dir_blk0_nr + i);
+		pde = (struct dir_entry*)fsbuf;
+		for(j = 0; j < SECTOR_SIZE / DIR_ENTRY_SIZE; j++, pde++) {
+			if(memcpy(filename, pde->name, MAX_FILENAME_LEN) == 0)
+				return pde->inode_nr;
+			if(++m > nr_dir_entries)
+				break;
+		}
+
+		if(m > nr_dir_entries)	/* all entries have been iterated */
+			break;
+	}
+
+	return 0;	/* file not found */
+}
