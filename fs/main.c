204c088c03da0a31b21c0755318168932c4812da
OneCodeMonkey <1460018362@qq.com>
2019-07-15 03:21:51 +0800

    fileSystem/main.c: fs_exit()

--- fs/main.c
+++ fs/main.c
@@ -73,91 +73,103 @@ PRIVATE void mkfs()
  * @return Zero if success.
  *
  */
- PUBLIC int rw_sector(int io_type, int dev, u64 pos, int bytes, int proc_nr, void* buf)
- {
- 	// todo
- } 
+PUBLIC int rw_sector(int io_type, int dev, u64 pos, int bytes, int proc_nr, void* buf)
+{
+	// todo
+}
 
- /**
-  * read_super_block()
-  *
-  * <Ring 1> Read super block from the given device then write it into a free super_block[] slot.
-  *
-  * @param dev: From which device the super block comes.
-  *
-  */
- PRIVATE void read_super_block(int dev)
- {
- 	// todo
- }
+/**
+ * read_super_block()
+ *
+ * <Ring 1> Read super block from the given device then write it into a free super_block[] slot.
+ *
+ * @param dev: From which device the super block comes.
+ *
+ */
+PRIVATE void read_super_block(int dev)
+{
+	// todo
+}
 
- /**
-  * get_super_block()
-  *
-  * <Ring 1> Get the super block from super_block[]
-  * @param dev: Device nr.
-  * @return  The ptr of super block.
-  *
-  */
- PUBLIC struct super_block* get_super_block(int dev)
- {
- 	// todo
- }
+/**
+ * get_super_block()
+ *
+ * <Ring 1> Get the super block from super_block[]
+ * @param dev: Device nr.
+ * @return  The ptr of super block.
+ *
+ */
+PUBLIC struct super_block* get_super_block(int dev)
+{
+	// todo
+}
 
- /**
-  * get_inode()
-  *
-  * <Ring 1> Get the inode ptr of given inode nr. A cache -- inode_table[] -- is maintained to make things faster.
-  * If the inode requested is already there, just return it.
-  * Otherwise the inode will be read from the disk.
-  *
-  * @param dev: Device nr.
-  * @param num: I-node nr.
-  * @return  The inode ptr requested.
-  *
-  */
- PUBLIC struct inode* get_inode(int dev, int num)
- {
- 	// todo
- }
+/**
+ * get_inode()
+ *
+ * <Ring 1> Get the inode ptr of given inode nr. A cache -- inode_table[] -- is maintained to make things faster.
+ * If the inode requested is already there, just return it.
+ * Otherwise the inode will be read from the disk.
+ *
+ * @param dev: Device nr.
+ * @param num: I-node nr.
+ * @return  The inode ptr requested.
+ *
+ */
+PUBLIC struct inode* get_inode(int dev, int num)
+{
+	// todo
+}
 
- /**
-  * put_inode()
-  *
-  * Decrease the reference nr of a slot in inode_table[]. When the nr
-  * reaches zero, it means the inode is not used any more and can be overwritten by a new inode.
-  *
-  * @param pinode: I-node ptr.
-  *
-  */
- PUBLIC void put_inode(struct inode* pinode)
- {
- 	// todo
- }
+/**
+ * put_inode()
+ *
+ * Decrease the reference nr of a slot in inode_table[]. When the nr
+ * reaches zero, it means the inode is not used any more and can be overwritten by a new inode.
+ *
+ * @param pinode: I-node ptr.
+ *
+ */
+PUBLIC void put_inode(struct inode* pinode)
+{
+	// todo
+}
 
- /**
-  * sync_inode()
-  *
-  * <Ring 1> Write the inode back to the disk. Commonly invoked as soon as the inode is changed.
-  *
-  * @param p: I-node ptr.
-  *
-  */
- PUBLIC void sync_inode(struct inode* p)
- {
- 	// todo
- }
+/**
+ * sync_inode()
+ *
+ * <Ring 1> Write the inode back to the disk. Commonly invoked as soon as the inode is changed.
+ *
+ * @param p: I-node ptr.
+ *
+ */
+PUBLIC void sync_inode(struct inode* p)
+{
+	// todo
+}
 
- /**
-  * fs_fork()
-  *
-  * Perform the aspects of fork() that relate to files.
-  *
-  * @return Zero if success, -1 otherwise.
-  *
-  */
- PRIVATE int fs_fork()
- {
- 	// todo
- }
- 
+/**
+ * fs_fork()
+ *
+ * Perform the aspects of fork() that relate to files.
+ *
+ * @return Zero if success, -1 otherwise.
+ *
+ */
+PRIVATE int fs_fork()
+{
+	// todo
+}
+
+/**
+ * fs_exit()
+ *
+ * Perform the aspects of exit() that relate to files.
+ *
+ * @return Zero if success.
+ *
+ */
+PRIVATE int fs_exit()
+{
+	// todo
+}
